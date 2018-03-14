3a369a9e6ff09739c1ed718074cecea4673b14f0
wenwenlife <1216858352@qq.com>
2018-03-14 17:36:28 +0800

    1.0.0

---
+++ src/main/java/com/xe/demo/common/annotation/Authority.java
@@ -0,0 +1,21 @@
+package com.xe.demo.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 权限注解
+ * 
+ * @author LW
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Authority {
+	String opCode();
+
+	String opName();
+
+	int opSeq() default 1;
+}
